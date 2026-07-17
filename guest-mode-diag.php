b94065513a502ef8b129fdf6d4c7410b17beccf5
Talal Eddaouahiri <267345223+talalnewjersey-ops@users.noreply.github.com>
2026-07-17 15:23:43 -0400

    Write action: add wp-i18n to LiteSpeed's JS Defer exclude list, to fix the wp-is-not-defined race (wp-i18n-js-after was deferred out of order relative to the wp-i18n library script that defines window.wp)

--- guest-mode-diag.php
+++ guest-mode-diag.php
@@ -1,29 +1,16 @@
 <?php
 add_action('rest_api_init', function () {
-    register_rest_route('mag-diag/v1', '/lscache-jsexc2', [
-        'methods' => 'GET',
+    register_rest_route('mag-diag/v1', '/lscache-jsexc-fix', [
+        'methods' => 'POST',
         'callback' => function () {
-            $out = [];
-            if (class_exists('\LiteSpeed\Base')) {
-                $ref = new ReflectionClass('\LiteSpeed\Base');
-                $consts = $ref->getConstants();
-                foreach ($consts as $k => $v) {
-                    if (stripos($k, 'JS') !== false && (stripos($k, 'EXC') !== false || stripos($k, 'DELAY') !== false || stripos($k, 'DEFER') !== false)) {
-                        $out['const_' . $k] = $v;
-                    }
-                }
+            $before = get_option('litespeed.conf.optm-js_defer_exc');
+            $new = is_array($before) ? $before : [];
+            if (!in_array('wp-i18n', $new, true)) {
+                $new[] = 'wp-i18n';
             }
-            if (class_exists('\LiteSpeed\Conf')) {
-                $conf = \LiteSpeed\Conf::get_instance();
-                foreach ($out as $constName => $optKey) {
-                    $shortConst = str_replace('const_', '', $constName);
-                    if (defined("\LiteSpeed\Base::$shortConst")) {
-                        $val = $conf->conf(constant("\LiteSpeed\Base::$shortConst"));
-                        $out['value_' . $shortConst] = $val;
-                    }
-                }
-            }
-            return $out;
+            $updated = update_option('litespeed.conf.optm-js_defer_exc', $new);
+            $after = get_option('litespeed.conf.optm-js_defer_exc');
+            return ['before' => $before, 'after' => $after, 'update_result' => $updated];
         },
         'permission_callback' => '__return_true',
     ]);
