3f1cda4b21c0bb9d97bbcad692d6d8318fbd70b6
Talal Eddaouahiri <267345223+talalnewjersey-ops@users.noreply.github.com>
2026-07-17 15:21:17 -0400

    Diagnostic v2: enumerate all JS delay/defer/exclude constants via reflection to find the real option keys

--- guest-mode-diag.php
+++ guest-mode-diag.php
@@ -1,12 +1,27 @@
 <?php
 add_action('rest_api_init', function () {
-    register_rest_route('mag-diag/v1', '/lscache-jsexc', [
+    register_rest_route('mag-diag/v1', '/lscache-jsexc2', [
         'methods' => 'GET',
         'callback' => function () {
             $out = [];
-            $keys = ['optm-js_exc', 'optm-js_defer_exc', 'optm-js_delay_exc'];
-            foreach ($keys as $k) {
-                $out[$k] = get_option("litespeed.conf.$k");
+            if (class_exists('\LiteSpeed\Base')) {
+                $ref = new ReflectionClass('\LiteSpeed\Base');
+                $consts = $ref->getConstants();
+                foreach ($consts as $k => $v) {
+                    if (stripos($k, 'JS') !== false && (stripos($k, 'EXC') !== false || stripos($k, 'DELAY') !== false || stripos($k, 'DEFER') !== false)) {
+                        $out['const_' . $k] = $v;
+                    }
+                }
+            }
+            if (class_exists('\LiteSpeed\Conf')) {
+                $conf = \LiteSpeed\Conf::get_instance();
+                foreach ($out as $constName => $optKey) {
+                    $shortConst = str_replace('const_', '', $constName);
+                    if (defined("\LiteSpeed\Base::$shortConst")) {
+                        $val = $conf->conf(constant("\LiteSpeed\Base::$shortConst"));
+                        $out['value_' . $shortConst] = $val;
+                    }
+                }
             }
             return $out;
         },
