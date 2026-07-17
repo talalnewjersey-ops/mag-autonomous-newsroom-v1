38e6f9aa667c331711a535e050da97fcf9160835
Talal Eddaouahiri <267345223+talalnewjersey-ops@users.noreply.github.com>
2026-07-16 21:23:16 -0400

    Guest Mode disable snippet: update_option litespeed.conf.guest/guest_optm/optm-guest_only to 0, exposed via POST route, user-authorized

--- guest-mode-diag.php
+++ guest-mode-diag.php
@@ -1,18 +1,22 @@
 <?php
 add_action('rest_api_init', function () {
-    register_rest_route('mag-diag/v1', '/lscache-guest', [
-        'methods' => 'GET',
+    register_rest_route('mag-diag/v1', '/lscache-guest-disable', [
+        'methods' => 'POST',
         'callback' => function () {
-            $keys = [
-                'litespeed.conf.guest',
-                'litespeed.conf.guest_optm',
-                'litespeed.conf.optm-guest_only',
+            $before = [
+                'litespeed.conf.guest' => get_option('litespeed.conf.guest'),
+                'litespeed.conf.guest_optm' => get_option('litespeed.conf.guest_optm'),
+                'litespeed.conf.optm-guest_only' => get_option('litespeed.conf.optm-guest_only'),
             ];
-            $out = [];
-            foreach ($keys as $k) {
-                $out[$k] = get_option($k);
-            }
-            return $out;
+            update_option('litespeed.conf.guest', '0');
+            update_option('litespeed.conf.guest_optm', '0');
+            update_option('litespeed.conf.optm-guest_only', '0');
+            $after = [
+                'litespeed.conf.guest' => get_option('litespeed.conf.guest'),
+                'litespeed.conf.guest_optm' => get_option('litespeed.conf.guest_optm'),
+                'litespeed.conf.optm-guest_only' => get_option('litespeed.conf.optm-guest_only'),
+            ];
+            return ['before' => $before, 'after' => $after];
         },
         'permission_callback' => '__return_true',
     ]);
